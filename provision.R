f92aa01aaf3708b2596fdcab2c0ccfff40dd70c3
RJSheppard <rjs11@ic.ac.uk>
2024-08-13 14:51:47 +0100

    Reverting to previous commit.

--- provision.R
+++
@@ -1,6 +0,0 @@
-
-install.packages("orderly2",repos = c("https://mrc-ide.r-universe.dev", "https://cloud.r-project.org"))
-install.packages("drjacoby")
-install.packages("dplyr")
-install.packages("ggplot2")
-
